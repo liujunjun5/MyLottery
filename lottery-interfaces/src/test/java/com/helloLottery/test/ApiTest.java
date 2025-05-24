392e1cb64ef70d9d6e91df8c6f842e17c48507af
刘俊 <1061999312@qq.com>
2025-05-24 18:33:36 +0800

    test:发奖逻辑的接口测试

--- lottery-interfaces/src/test/java/com/helloLottery/test/ApiTest.java
+++ lottery-interfaces/src/test/java/com/helloLottery/test/ApiTest.java
@@ -1,7 +1,14 @@
 package com.helloLottery.test;
 
+import com.helloLottery.domain.award.service.factory.DistributionGoodsFactory;
+import com.hellolottery.common.Constants;
 import com.alibaba.fastjson.JSON;
+import com.helloLottery.domain.award.model.req.GoodsReq;
+import com.helloLottery.domain.award.model.res.DistributionRes;
+import com.helloLottery.domain.award.service.goods.IDistributionGoods;
 import com.helloLottery.domain.strategy.model.req.DrawReq;
+import com.helloLottery.domain.strategy.model.res.DrawResult;
+import com.helloLottery.domain.strategy.model.vo.DrawAwardInfo;
 import com.helloLottery.domain.strategy.service.draw.IDrawExec;
 import com.helloLottery.infrastructure.dao.IActivityDao;
 import com.helloLottery.infrastructure.po.Activity;
@@ -54,6 +61,31 @@ public class ApiTest {
         drawExec.doDrawExec(new DrawReq("lj", 10001L));
     }
 
+    @Resource
+    private DistributionGoodsFactory distributionGoodsFactory;
+
+    @Test
+    public void test_award() {
+        // 执行抽奖
+        DrawResult drawResult = drawExec.doDrawExec(new DrawReq("刘俊", 10001L));
+
+        // 判断抽奖结果
+        Integer drawState = drawResult.getDrawState();
+        if (Constants.DrawState.FAIL.getCode().equals(drawState)) {
+            logger.info("未中奖 DrawAwardInfo is null");
+            return;
+        }
+
+        // 封装发奖参数，orderId：2109313442431 为模拟ID，需要在用户参与领奖活动时生成
+        DrawAwardInfo drawAwardInfo = drawResult.getDrawAwardInfo();
+        GoodsReq goodsReq = new GoodsReq(drawResult.getuId(), "2109313442431", drawAwardInfo.getAwardId(), drawAwardInfo.getAwardName(), drawAwardInfo.getAwardContent());
+
+        // 根据 awardType 从抽奖工厂中获取对应的发奖服务
+        IDistributionGoods distributionGoodsService = distributionGoodsFactory.getDistributionGoodsService(drawAwardInfo.getAwardType());
+        DistributionRes distributionRes = distributionGoodsService.doDistribution(goodsReq);
+
+        logger.info("测试结果：{}", JSON.toJSONString(distributionRes));
+    }
 }
 
 //class Draw
