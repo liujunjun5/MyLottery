a8a366a59a0702d7d65fed47f006b4aeb62c656a
刘俊 <1061999312@qq.com>
2025-06-08 21:50:22 +0800

    test: 抽奖rpc接口单元测试

---
+++ lottery-interfaces/src/test/java/com/helloLottery/test/LotteryActivityBoothTest.java
@@ -0,0 +1,41 @@
+package com.helloLottery.test;
+
+import com.alibaba.fastjson.JSON;
+import com.helloLottery.rpc.ILotteryActivityBooth;
+import com.helloLottery.rpc.req.DrawReq;
+import com.helloLottery.rpc.res.DrawRes;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * @author liujun
+ * @description:
+ * @date 2025/6/8 20:59
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LotteryActivityBoothTest {
+
+    private Logger logger = LoggerFactory.getLogger(LotteryActivityBoothTest.class);
+
+    @Resource
+    private ILotteryActivityBooth lotteryActivityBooth;
+
+    @Test
+    public void test_doDraw() {
+        DrawReq drawReq = new DrawReq();
+        drawReq.setuId("liujun");
+        drawReq.setActivityId(100001L);
+        DrawRes drawRes = lotteryActivityBooth.doDraw(drawReq);
+        logger.info("请求参数：{}", JSON.toJSONString(drawReq));
+        logger.info("测试结果：{}", JSON.toJSONString(drawRes));
+    }
+
+
+}
