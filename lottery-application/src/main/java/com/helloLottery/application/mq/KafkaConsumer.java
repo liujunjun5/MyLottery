343c33df3ec0d949696a7e52e34fb9ae695544c8
刘俊 <1061999312@qq.com>
2025-06-09 10:48:29 +0800

    feat:配置kafka环境

---
+++ lottery-application/src/main/java/com/helloLottery/application/mq/KafkaConsumer.java
@@ -0,0 +1,34 @@
+package com.helloLottery.application.mq;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+/**
+ * @author liujun
+ * @description: 消息消费者
+ * @date 2025/6/9 10:17
+ */
+@Component
+public class KafkaConsumer {
+
+    private Logger logger = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    @KafkaListener(topics = KafkaProducer.TOPIC_TEST, groupId = KafkaProducer.TOPIC_GROUP)
+    public void topicTest(ConsumerRecord<?, ?> record, Acknowledgment ack, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+        Optional<?> message = Optional.ofNullable(record.value());
+        if (message.isPresent()) {
+            Object msg = message.get();
+            logger.info("topic_test 消费了： Topic:" + topic + ",Message:" + msg);
+            ack.acknowledge();
+        }
+    }
+
+}
--- lottery-rpc/src/main/java/com/helloLottery/rpc/IAcitivityBooth.java
+++
@@ -1,8 +0,0 @@
-package com.helloLottery.rpc;
-
-import com.helloLottery.rpc.req.ActivityReq;
-import com.helloLottery.rpc.res.ActivityRes;
-
-public interface IAcitivityBooth {
-    ActivityRes queryActivityById(ActivityReq req);
-}
